5bc0b1472cefea1178805a5892797f756533ee48
ALI <68592145+Ali-awesome@users.noreply.github.com>
2022-05-20 11:24:47 +0600

    delete button in myitems

--- src/Component/ManageAllItems/ManageItems.js
+++ src/Component/ManageAllItems/ManageItems.js
@@ -1,17 +1,41 @@
 import React from 'react';
+import { toast } from 'react-toastify';
+import useItems from '../Hooks/useItems';
 
 const ManageItems = ({ property }) => {
-    const { name, url, description, price, supplier, _id } = property;
+    const [items, setItems] = useItems();
+    const { name, url, description, price, supplier, quantity, _id } = property;
+    const handleDelete = id => {
+        const confirmation = window.confirm('Are you sure?');
+        if (confirmation) {
+            const url = `http://localhost:5000/inventory/${id}`;
+            fetch(url, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data);
+                    const remaining = items.filter(item => item._id !== id);
+                    if (data.deletedCount === 1) {
+                        setItems(remaining);
+                        toast('Item Deleted Successfully!');
+                    }
+                });
+        };
+    }
     return (
         <div className='col item'>
             <div className="card h-100">
                 <div className="card-body text-dark text-center">
                     <img src={url} className="image card-img-top" alt="..."></img>
                     <h5 className="card-title">Name: {name}</h5>
-                    {/* <div>
-                        <button onClick={() => navigateToDetails()} className='mb-2 btn btn-dark'>Add: {name}</button>
-                        <button onClick={() => navigateToDetails(_id)} className='btn btn-dark'>Update: {name}</button>
-                    </div> */}
+                    <p className='m-0'><b>Price:</b> ${price}</p>
+                    <p className='m-0'><b>Quantity:</b> {quantity}</p>
+                    <p className="m-0"><small>{description}</small></p>
+                    <p><b>Supplier:</b> {supplier}</p>
+                    <div>
+                        <button onClick={() => handleDelete(_id)} className='mb-2 btn btn-dark'>Delete This Item</button>
+                    </div>
                 </div>
             </div>
         </div>
--- src/Component/MyItem/MyItem.js
+++ src/Component/MyItem/MyItem.js
@@ -35,24 +35,24 @@ const MyItem = () => {
 
 
 
-    // const handleDelete = id => {
-    //     const confirmation = window.confirm('Are you sure?');
-    //     if (confirmation) {
-    //         const url = `http://localhost:5000/myItems/${id}`;
-    //         fetch(url, {
-    //             method: 'DELETE'
-    //         })
-    //             .then(res => res.json())
-    //             .then(data => {
-    //                 console.log(data);
-    //                 const remaining = myItems.filter(item => item._id !== id);
-    //                 if (data.deletedCount === 1) {
-    //                     setMyItems(remaining);
-    //                     toast('Item Deleted Successfully!');
-    //                 }
-    //             });
-    //     };
-    // }
+    const handleDelete = id => {
+        const confirmation = window.confirm('Are you sure?');
+        if (confirmation) {
+            const url = `http://localhost:5000/myItems/${id}`;
+            fetch(url, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data);
+                    const remaining = myItems.filter(item => item._id !== id);
+                    if (data.deletedCount === 1) {
+                        setMyItems(remaining);
+                        toast('Item Deleted Successfully!');
+                    }
+                });
+        };
+    }
     return (
         <div>
             <h2 className='warehouse fs-1'>You heve {myItems.length} items.</h2>
@@ -65,9 +65,9 @@ const MyItem = () => {
                                 <h5 className="card-title">Name: {item.name}</h5>
                                 <p className='m-0'><b>Price:</b> {item.price}</p>
                                 <p className="m-0"><small>{item.MyItemdescription}</small></p>
-                                {/* <div>
+                                <div>
                                     <button onClick={() => handleDelete(item._id)} className='mb-2 btn btn-dark'>Delete This Item</button>
-                                </div> */}
+                                </div>
                             </div>
                         </div>
                     </div>)
